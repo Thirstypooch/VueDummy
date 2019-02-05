6185f24080d8b074f32ebbd509d15ead9531d34c
Thirstypooch <juancocchi6@gmail.com>
2019-02-05 11:08:44 -0500

    Add files via upload

---
+++ src/components/Shared/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="row">
+    <div class="col-xs-12">
+      <footer>
+        <p>All servers are being managed now</p>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
